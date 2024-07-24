f57951170cc8f65cc624521b5cab8351ad84d07a
Mahedere <mahederetebebe98@gmail.com>
2024-07-24 11:45:55 -0500

    input validity completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     expectedReturn: 6,
     duration: 10
   })
-
+  const inputIsValid = userInput.duration >= 1
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
       return {
@@ -23,7 +23,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a positive duration</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   )
 }
