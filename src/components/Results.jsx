08998b1324298a8776321194801bf3d2f337cbca
Mahedere <mahederetebebe98@gmail.com>
2024-07-24 11:00:38 -0500

    results functionality completed

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,6 @@
+import { calculateInvestmentResults } from "../util/investment"
 export default function Results({input}) {
-    console.log(input)
+    const resultsData=calculateInvestmentResults(input)
     return <>
     </>
 }
